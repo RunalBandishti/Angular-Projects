a736b32eb2d76b0ac333008b0614b3ca9fd7a41d
Runal <bandishtirunal@gmail.com>
2022-02-25 20:47:55 +0530

    add photos

---
+++ photos-app/src/app/photos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotosService {
+  constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization:
+            'Client-ID 1oQv94BpJF-2AgEVkZFKPq1DAczLaoUhdAcleyzs-Ms',
+        },
+      }
+    );
+  }
+}
