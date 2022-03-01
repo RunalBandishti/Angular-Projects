2c27170dac11335248f293524b970468e284b6bf
Runal <bandishtirunal@gmail.com>
2022-03-01 17:17:02 +0530

    add word

---
+++ wsearch/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  pages = [];
+  constructor(private wikipedia: WikipediaService) {}
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    });
+  }
+}
+
+// https://en.wikipedia.org/w/api.php?
+// action = query &
+//   format=json &
+//   list=search &
+//   utf8=1 &
+//   srsearch=space
---
+++ wsearch/src/app/page-list/page-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list',
+  templateUrl: './page-list.component.html',
+  styleUrls: ['./page-list.component.css']
+})
+export class PageListComponent implements OnInit {
+
+  @Input() pages = <any>[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
